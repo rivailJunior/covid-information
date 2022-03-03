264712ed941206527215e730669cef619b688e4b
rivail.santos <rivail.santos@olist.com>
2022-03-03 00:36:19 -0400

    feat: add leaflet map

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useEffect } from "react";
+
+import styled from "styled-components/macro";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+const { REACT_APP_MAPBOX_KEY } = process.env;
+
+const MapWrapper = styled(MapContainer)`
+  height: 360px;
+  background-color: black;
+`;
+
+const position = [51.505, -0.09];
 
 const Home = () => {
   const handleOnClick = () => {
-    window.location.assign('/about');
+    window.location.assign("/about");
   };
 
   return (
     <div>
-      <h2>
-        Home page
-      </h2>
+      <h2>Home page</h2>
       <button onClick={handleOnClick}>Click here to load page</button>
+      <MapWrapper center={position} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${REACT_APP_MAPBOX_KEY}`}
+          id="mapbox/streets-v11"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapWrapper>
     </div>
-
   );
 };
 
