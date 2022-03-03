4af4b5e5bb1742728802cb727cfabea2c3a8631d
rivail.santos <rivail.santos@olist.com>
2022-03-03 16:03:53 -0400

    fix: remove unnecessary code

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,6 @@ import React, { useEffect } from "react";
 import MapContent from "../components/map/map";
 
 const Home = () => {
-  const handleOnClick = () => {
-    window.location.assign("/about");
-  };
-
   return (
     <div>
       <h2>Home page</h2>
