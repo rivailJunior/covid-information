76833920067a4100ceb29da5be4e7d16cde72d88
rivail.santos <rivail.santos@olist.com>
2022-05-18 23:06:44 -0400

    remove: no used code

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-
-import HeaderAppBar from "../components/header/header";
-import { Container, Toolbar, Button } from "@mui/material";
+import { Container } from "@mui/material";
 import CssBaseline from "@mui/material/CssBaseline";
 import Pages from "./routes-pages";
+
+import HeaderAppBar from "../components/header/header";
 
 export default function AppRoutes() {
   return (
